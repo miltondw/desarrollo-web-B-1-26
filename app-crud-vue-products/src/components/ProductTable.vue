e29ef2c2645f17569717cdb4a8ebbf1add1b99ad
miltondw <estradamilton2001@gmail.com>
2026-05-20 20:14:54 -0500

    curd con vue y mock api

---
+++ app-crud-vue-products/src/components/ProductTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>Nombre</th><th>Precio</th><th>Descripción</th><th>Categoría</th><th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="p in products" :key="p.id">
+        <td>{{p.title}}</td>
+        <td>{{p.price}}</td>
+        <td>{{p.description}}</td>
+        <td>{{p.category}}</td>
+        <td>
+          <button class="btn btn-primary btn-sm" @click="$emit('edit',p.id)">Editar</button>
+          <button class="btn btn-danger btn-sm ms-2" @click="$emit('delete',p.id)">Eliminar</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default { props:['products'] }
+</script>
